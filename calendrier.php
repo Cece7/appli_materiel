1812abaceb38f61b7ad6790ccac83753397ddec8
Jujuherrg <32415525+Jujuherrg@users.noreply.github.com>
2017-11-10 11:33:54 +0100

    Add files via upload

--- calendrier.php
+++ calendrier.php
@@ -2,4 +2,19 @@
 echo $_POST["day"];
 echo $_POST["month"];
 echo $_POST["year"];
+
+try
+{
+	$id_connex=new PDO('mysql:host=localhost;dbname=ptut','root','',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+}
+catch (Exception $e)
+{
+	die('Erreur : ' . $e->getMessage());
+}
+$reponse=$id_connex->query('SELECT reference FROM materiel');
+$ligne=$reponse->fetch(PDO::FETCH_ASSOC);
+var_dump($ligne);
+$reponse->closeCursor();
+$id_connex=null;
+
 ?>
