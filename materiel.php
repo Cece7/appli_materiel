a767c13a99dd30521de892dc8499c6a5360895fb
Rohrhurst_Marco <32276415+Yuraken@users.noreply.github.com>
2017-11-20 10:52:20 +0100

    Add files via upload
    MàJ

--- materiel.php
+++ materiel.php
@@ -1,28 +1,46 @@
- 	<form method="post" action="<?php echo $_SERVER['PHP_SELF']?>">
+ 	<meta charset="utf-8">
+    <form method="post" action="<?php echo $_SERVER['PHP_SELF']?>">
 
-	<h1>Réservation :</h1>
-</br>
-     <b>Date Début: <b> <input type="text" name="date-debut" value="">
+
+    <legend><h1>Réservation :</h1></legend> 
+ </br>
+     <label for="date-debut"><b>Date de réservation: </b></label> <input type="date" name="date-debut" value=""> au <input type="date" name="date-fin" value="">
  </br></br>
-     <b>Date Fin: <b> <input type="text" name="date-fin" value="">
+     <label for="nom"><b>Nom: </b></label> <input type="text" name="nom" value="">
  </br></br>
-     <b>Nom du responsable: <b> <input type="text" name="nom" value="">
+    <label for="prenom"><b>Prénom: </b></label> <input type="text" name="prenom" value="">
+ </br></br>
+    <label for="groupe"><b>Groupe: </b></label> <select>
+                                                    <option selected="selected" value="null">Groupe</option>
+                                                    <option value="P11">P11</option>
+                                                    <option value="P12">P12</option>
+                                                    <option value="P21">P21</option>
+                                                    <option value="P22">P22</option>
+                                                </select>
+                                                <select>
+                                                    <option selected="selected" value="null">Année</option>
+                                                    <option value="1">1</option>
+                                                    <option value="2">2</option>
+
+                                                </select>
  </br></br>
      <b>Matériel: <b> 
  </br></br>
+ 
  <div class="displaywrap">
  	<div id="choix"><b>Vidéo</b>
+
  		<br>
  		Caméra 1: 
  			<select>
- 				<option selected="selected">Nombre</option>
+ 				<option selected="selected" value="null">Nombre</option>
  				<option value="1">1</option>
  				<option value="2">2</option>
  			</select>
  		<br>
  		Caméra 2: 
  			<select>
- 				<option selected="selected">Nombre</option>
+ 				<option selected="selected" value="null">Nombre</option>
  				<option value="1">1</option>
  				<option value="2">2</option>
  			</select>
@@ -32,14 +50,14 @@
  		<br>
  		Micro 1: 
  		<select>
- 				<option selected="selected">Nombre</option>
+ 				<option selected="selected" value="null">Nombre</option>
  				<option value="1">1</option>
  				<option value="2">2</option>
  		</select>
  		<br>
  		Micro 2: 
  		<select>
- 				<option selected="selected">Nombre</option>
+ 				<option selected="selected" value="null">Nombre</option>
  				<option value="1">1</option>
  				<option value="2">2</option>
  		</select>
@@ -48,14 +66,14 @@
  		<br>
  		Accessoire 1: 
  		<select>
- 				<option selected="selected">Nombre</option>
+ 				<option selected="selected" value="null">Nombre</option>
  				<option value="1">1</option>
  				<option value="2">2</option>
  		</select>
  		<br>
  		Accessoire 2: 
  		<select>
- 				<option selected="selected">Nombre</option>
+ 				<option selected="selected" value="null">Nombre</option>
  				<option value="1">1</option>
  				<option value="2">2</option>
  		</select>
@@ -64,14 +82,14 @@
  		<br>
  		Lumière 1: 
  		<select>
- 				<option selected="selected">Nombre</option>
+ 				<option selected="selected" value="null">Nombre</option>
  				<option value="1">1</option>
  				<option value="2">2</option>
  		</select>
  		<br>
  		Lumière 2: 
  		<select>
- 				<option selected="selected">Nombre</option>
+ 				<option selected="selected" value="null">Nombre</option>
  				<option value="1">1</option>
  				<option value="2">2</option>
  		</select>
@@ -96,3 +114,44 @@
     }
 
  </style>
+ <?php 
+if (isset($_POST["submit"])) {
+ try
+{
+    $id_connex=new PDO('mysql:host=localhost;dbname=ptut','root','',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+}
+catch (Exception $e)
+{
+    die('Erreur : ' . $e->getMessage());
+}
+$reponse=$id_connex->query('SELECT reference FROM materiel');
+
+$nb_mat=$reponse->rowCount();
+if($nb_mat==0){
+    echo "Pas de matériel";
+}
+else{
+    while($ligne=$reponse->fetch(PDO::FETCH_ASSOC))
+    {
+        echo $ligne['reference'];
+    }
+}
+
+
+$reponse->closeCursor();
+$id_connex=null;
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+ ?>
